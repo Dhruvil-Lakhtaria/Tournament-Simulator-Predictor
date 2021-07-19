4adde3f815f0d605e36e7ef92fd73e28a219dc3c
Dhruvil <lakhtariadhruvil123@gmail.com>
2021-07-19 23:52:59 +0530

    scheduling done

---
+++ src/GroupStage.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class GroupStage implements TournamentStage {
+	ArrayList<Team>playingTeams;
+	ArrayList<Match>matches;
+	ArrayList<Row>pointsTable;
+	
+//	Constructor
+	public GroupStage(ArrayList<Team>t)
+	{
+		this.teams = t;
+		matches = new ArrayList<Match>();
+		pointsTable = new ArrayList<Row>();
+	}
+//	Filling matches ARRAYLIST with scheduled fixtures...
+	@Override
+	public void schedule() {
+//		here everywhere number represent the index of team in arraylist
+		/*
+		 * first setting teams consecutively
+		 * like (0,1) , (2,3) , (4,0) , (1,2) ,(3,4)
+		 * 5 matches done
+		 */
+		for(int i = 0;i<=8;i+=2)
+		{
+			matches.add(new Match(teams.get(i%5),teams.get((i+1)%5)));
+		}
+		/*
+		 * setting teams alternatively 
+		 * like 0,2....1,3.....2,4....3,0.....4,1
+		 * 10 matches done
+		 */
+		for(int i = 0;i<=4;i++)
+		{
+			matches.add(new Match(teams.get(i%5),teams.get((i+2)%5)));
+		}
+		/*
+		 * final schedule ---
+		 * 0,1 -> 2,3 -> 4,0 -> 1,2 -> 3,4 -> 0,2 -> 1,3 -> 2,4 -> 3,0 -> 4,1 
+		 */
+	}
+
+	@Override
+	public void simulate() {
+		
+	}
+
+	@Override
+	public ArrayList<Player> getGoalScorers() {
+		
+		return null;
+	}
+
+}
