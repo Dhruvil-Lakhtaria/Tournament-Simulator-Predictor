a8ba40ade3016ce72405c4c4c0e51fa5b44e7a60
Advaith Curpod <45689635+advaithcurpod@users.noreply.github.com>
2021-07-19 19:39:56 +0530

    updated Knockouts.java, implemented schedule()
    as of now, as @abdullah1308 told, I have removed the qualified teams array, cause even I think, there is no need for that, we can directly make changes to playingTeams array... but incase, as @not-shoyo told, if we need a separate array, and we have a strong reason why, I will create a separate field.

--- src/Knockouts.java
+++ src/Knockouts.java
@@ -3,40 +3,14 @@ import java.util.ArrayList;
 public class Knockouts implements TournamentStage{
     private ArrayList<Team> playingTeams;
     private ArrayList<Match> matches;
-    private ArrayList<Team> qualified;
+    private ArrayList<Player> goalScorers;
 
     /**constructor*/
     Knockouts(ArrayList<Team> playingTeams){
         this.playingTeams = playingTeams;
+        matches = new ArrayList<>();
+        goalScorers = new ArrayList<>();
     }
-
-    /**getters & setters*/
-    public ArrayList<Team> getPlayingTeams() {
-        return playingTeams;
-    }
-
-    public void setPlayingTeams(ArrayList<Team> playingTeams) {
-        this.playingTeams = playingTeams;
-    }
-
-
-    public ArrayList<Match> getMatches() {
-        return matches;
-    }
-
-    public void setMatches(ArrayList<Match> matches) {
-        this.matches = matches;
-    }
-
-
-    public ArrayList<Team> getQualified() {
-        return qualified;
-    }
-
-    public void setQualified(ArrayList<Team> qualified) {
-        this.qualified = qualified;
-    }
-
 
     /**methods implemented from Interface
      * schedule()
@@ -65,9 +39,70 @@ public class Knockouts implements TournamentStage{
 
     public void simulate(){
 
+        for(Match match : matches){
+            /**play the match*/
+            match.play();
+
+            /**delay*/
+            Delay.makeDelay(4_000);
+
+            /**update the playingTeams array
+             * basically, remove the losing team
+             * by the end of the loop, the playingTeams array will be of half the size*/
+            playingTeams.remove(match.getLoser());
+
+            /**update the goalScorer array
+             * add the goal scorers of the match (from both losing team & winning team)*/
+            goalScorers.addAll(match.getWinningTeamScorers());
+            goalScorers.addAll(match.getLosingTeamScorers());
+
+            /**print Match stats*/
+            System.out.println(match.toString());
+        }
+    }
+
+
+    /**toString()*/
+    public String toString() {
+        String s = "";
+
+        if(playingTeams.size() == 8)
+            s += "Quarter-Final\n";
+        else if(playingTeams.size() == 4)
+            s += "Semi-Final\n";
+        else
+            s += "Final\n";
+
+        for(Match match : matches){
+            s += match.getTeam1().getName() + " vs " + match.getTeam2().getName() + "\n";
+        }
+        return s;
+    }
+
+
+    /**getters & setters*/
+    public ArrayList<Team> getPlayingTeams() {
+        return new ArrayList<Team>(playingTeams);
+    }
+
+    public void setPlayingTeams(ArrayList<Team> playingTeams) {
+        this.playingTeams = playingTeams;
+    }
+
+    public ArrayList<Match> getMatches() {
+        return new ArrayList<>(matches);
+    }
+
+    public void setMatches(ArrayList<Match> matches) {
+        this.matches = matches;
     }
 
     public ArrayList<Player> getGoalScorers(){
-        
+        return new ArrayList<>(goalScorers);
     }
+
+    public void setGoalScorers(ArrayList<Player> goalScorers) {
+        this.goalScorers = goalScorers;
+    }
+
 }
